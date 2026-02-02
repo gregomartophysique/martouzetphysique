cde69c3b95770061e5e5928e3f73501dccf8363c
gregomartophysique <gregoire.martouzet@ac-dijon.fr>
2026-02-02 08:38:16 +0100

    ajout page code et application

--- docs/load_list.js
+++ docs/load_list.js
@@ -3,7 +3,7 @@ data = [
 			"name": "De Fresnel à Fraunhofer",
 			"img": "Fresnel_Fraun.jpg",
 			"codePy": "De_Fresnel_a_Fraun.py",
-			"keyword": ["Ondulatoire", "Optique"],
+			"keyword": ["Onde", "Optique"],
 			"type": "py",
 			"description": "Calcul de la figure de diffraction par intégration de Huygens-Fresnel. Possibilité de modifier certains paramètres (longueur d'onde, largeur de la fente, ...). Le nombre de Fresnel F est affiché en direct. L'éclairement 2D, tel qu'on le voit sur un écran et éclairement en fonction de la position"
 		},
@@ -11,7 +11,7 @@ data = [
 			"name": "Critère de Rayleigh",
 			"img": "Rayleigh.jpg",
 			"codePy": "Critere_Rayleigh.py",
-			"keyword": ["optiqueOndu"],
+			"keyword": ["Onde", "Optique"],
 			"type": "py",
 			"description": "Illustration du critère de Rayleigh. Possibilité de modifier certains paramètres (longueur d'onde, largeur de la fente, ...). L'éclairement 2D, tel qu'on le voit sur un écran et éclairement en fonction de la position"
 		},
@@ -19,7 +19,7 @@ data = [
 			"name": "Diffraction périodique",
 			"img": "diffraction_periodique.jpg",
 			"codePy": "Diffraction_periodique.py",
-			"keyword": ["optiqueOndu"],
+			"keyword": ["Onde", "Optique"],
 			"type": "py",
 			"description": ""
 		},
@@ -27,7 +27,7 @@ data = [
 			"name": "Trajectoire",
 			"img": "trajectoire.jpg",
 			"codePy": "Trajectoire.py",
-			"keyword": ["mecanique"],
+			"keyword": ["Mécanique"],
 			"type": "py",
 			"description": "Trajectoire d'un point autour d'un centre et affichage de la zone d'énergie potentielle accessible."
 		},
@@ -36,7 +36,7 @@ data = [
 			"name": "Marée",
 			"img": "maree.jpg",
 			"codePy": "Maree.py",
-			"keyword": ["mecanique"],
+			"keyword": ["Mécanique"],
 			"type": "py",
 			"description": ""
 		},
@@ -44,7 +44,7 @@ data = [
 			"name": "Equation auto-cohérente",
 			"img": "Equ_autocoherente.jpg",
 			"codePy": "Equ_autocoherente.py",
-			"keyword": ["thermo"],
+			"keyword": ["Thermodynamique"],
 			"type": "py",
 			"description": "Résolution de l'équation auto-cohérente en champ moyen pour différentes températures et différents champs magnétiques."
 		},
@@ -52,7 +52,7 @@ data = [
 			"name": "Energie libre de Landau",
 			"img": "Energie_libre_Landau.jpg",
 			"codePy": "Energie_libre_Landau.py",
-			"keyword":[ "thermo"],
+			"keyword": [ "Thermodynamique"],
 			"type": "py",
 			"description": "Affichage de l'énergie libre dans le cas du model de Landau (pour différentes températures)."
 		},
@@ -61,7 +61,7 @@ data = [
 			"img": "loi_planck.jpg",
 			"codePy": "loi_planck.py",
 			"type": "py",
-			"keyword": ["thermo"],
+			"keyword": ["Thermodynamique"],
 			"description": "Calcul du spectre d'un corps à partir de la loi de Planck. Donne également des éléments tel que le maximum, la constante de Stefan, etc."
 		},
 		{
@@ -69,7 +69,7 @@ data = [
 			"img": "Cristallographie.jpg",
 			"code": "",
 			"type": "pa",
-			"keyword": ["chimie"],
+			"keyword": ["Chimie"],
 			"description": "Visualisation 3D de mailles."
 		},
 		{
@@ -78,7 +78,7 @@ data = [
 			"codePy": "Traitement_Signal.py",
 			"codeJS": "app_filtrage.html",
 			"type": "py",
-			"keyword": ["elec"],
+			"keyword": ["Filtrage", "Electricité", "Signaux"],
 			"description": "Visualisation de l'influence de filtre sur un signal, représentation temporelle et spectrale"
 		}
 
@@ -130,12 +130,22 @@ window.onload = function () {
 			key = document.createElement("div");
 			key.className = "keyword";
 			key.innerHTML = data[j].keyword[k];
-			possible_keywords.push(data[j].keyword[k]);
+			if (!possible_keywords.some(e => e === data[j].keyword[k])) {
+				possible_keywords.push(data[j].keyword[k]);
+			}
 			keywords.appendChild(key);
 		}
 
 		// Bouton pour les codes (ou liens)
 		var liste_btnCode = document.createElement("div");
+
+		btnCodeLink = document.createElement("A");
+		btnCode = document.createElement("BUTTON");
+		btnCodeLink.href = "./application_info.html?code_name=" + data[j].codePy;
+		btnCode.innerHTML = "En savoir plus";
+		btnCodeLink.appendChild(btnCode);
+		liste_btnCode.appendChild(btnCodeLink);
+
 
 		if (typeof data[j].codePy !== "undefined") {
 			btnCodeLink = document.createElement("A");
@@ -164,11 +174,19 @@ window.onload = function () {
 		liste.appendChild(element);
 	}
 
-	var choose_keyword = document.getElementById("choose_keyword");
+	var choose_keyword = document.getElementById("listCategorie");
 	for (let k = 0; k < possible_keywords.length; k++) {
-		key = document.createElement("div");
-		key.className = "keyword";
-		key.innerHTML = possible_keywords[k];
+		key = document.createElement("LI");
+		input = document.createElement("INPUT");
+		input.setAttribute('type', "checkbox");
+		input.setAttribute('value', possible_keywords[k]);
+		input.setAttribute('id', "checkitem_categorie_" + k);
+
+		input_label = document.createElement("LABEL");
+		input_label.setAttribute('for', "checkitem_categorie_" + k);
+		input_label.innerHTML = possible_keywords[k];
+		key.appendChild(input);
+		key.appendChild(input_label);
 		choose_keyword.appendChild(key);
 	}
 
